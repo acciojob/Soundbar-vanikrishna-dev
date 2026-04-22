f0459fd1ffbf40fde3f4f8183a9329b75dcd46c0
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-22 16:55:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const buttonsContainer = document.getElementById("buttons");
+
+const audioElements = {};
+
+sounds.forEach(sound => {
+	const audio = new Audio(`sounds/${sound}.mp3`);
+	audioElements[sound] = audio;
+});
+
+document.querySelectorAll(".btn").forEach(button => {
+	button.addEventListener("click", () => {
+		const sound = button.innerText;
+
+		if (button.classList.contains("stop")) {
+			stopAllSounds();
+		} else {
+			stopAllSounds();
+			audioElements[sound].play();
+		}
+	});
+});
+
+function stopAllSounds() {
+	for (let key in audioElements) {
+		audioElements[key].pause();
+		audioElements[key].currentTime = 0;
+	}
+}
